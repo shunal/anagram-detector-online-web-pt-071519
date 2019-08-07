0948060780e8f6f293e180b4d258d151b2b5881f
Steven Hunal <stevehunal@gmail.com>
2019-08-07 01:56:26 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,6 +9,7 @@ class Anagram
   
   def match(words)
     words.each do |word|
+      binding.pry
       split_word = word.split('')
       split_anogram = @word.split('')
     if split_word.sort == split_anogram.sort
