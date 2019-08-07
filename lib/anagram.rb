54da2de03a3c509b0d110f0242bf49a52f27fdf3
Steven Hunal <stevehunal@gmail.com>
2019-08-07 01:47:47 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,9 +9,10 @@ class Anagram
   
   def match(words)
     words.each do |word|
+      binding.pry
       split_word = word.split("")
     if split_word.sort == @word.sort
-      binding.pry
+      
       @word
       
     end
