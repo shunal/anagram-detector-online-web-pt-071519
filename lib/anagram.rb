f577e77725d69beafdbed26fb9ea413ee1fc9650
Steven Hunal <stevehunal@gmail.com>
2019-08-07 01:41:02 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(words)
+    words.each do |word|
+      word.split("")
+    if word.sort == @words.sort
+      @word
+     end
+    end
+end 
+  
+  
+  #word "toos"
+  
+  #words ["shoot" "kick" "soccer" "block"]
+  
+  
